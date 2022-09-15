8995b97b0e3b53fd56e97a6766bc113fed44ddc6
LukaKr <lk6134@student.uni-lj.si>
2022-09-15 20:05:30 +0200

    fix

--- client/src/App.js
+++ client/src/App.js
@@ -108,13 +108,13 @@ function App() {
 }
 
 const AppWithWalletProvider = () => (
+  <ConnectionProvider>
     <WalletProvider wallets={wallets}>
       <WalletModalProvider>
         <App/>
       </WalletModalProvider>
     </WalletProvider>
+  </ConnectionProvider>
 )
-
-
 
 export default AppWithWalletProvider;  
